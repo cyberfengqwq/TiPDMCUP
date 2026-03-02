126be4f79d3f3de4d6da15ef51c862689590e8e4
Yao0454 <yaoyifeng20070524@outlook.com>
2026-03-02 15:37:06 +0800

    Update

--- lib/llm.py
+++ lib/llm.py
@@ -6,26 +6,33 @@ from openai import OpenAI
 load_dotenv()
 
 
+class LLM:
+    def __init__(
+        self,
+        _api_key=os.getenv("DASHSCOPE_API_KEY"),
+        _base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+    ) -> None:
+        self.client = OpenAI(
+            api_key=_api_key,
+            base_url=_base_url,
+        )
+        self.prompt = ""
+        self.model = "qwen3.5-flash"
+        self.message: list = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": self.prompt},
+        ]
+
+    def chat(self, prompt: str) -> str:
+        self.prompt = prompt
+        completion = self.client.chat.completions.create(
+            model=self.model, messages=self.message
+        )
+        return str(completion.choices[0].message.content)
+
+
 def main() -> None:
-    prompt: str = "你好啊"
-
-    message: list = [
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": prompt},
-    ]
-    try:
-        client = OpenAI(
-            api_key=os.getenv("DASHSCOPE_API_KEY"),
-            base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
-        )
-
-        completion = client.chat.completions.create(
-            model="qwen3.5-flash", messages=message
-        )
-        print(completion.choices[0].message.content)
-
-    except Exception as e:
-        print(f"Error: {e}")
+    pass
 
 
 if __name__ == "__main__":
--- src/webAPI.py
+++ src/webAPI.py
@@ -1,6 +1,15 @@
+import uvicorn
 from fastapi import FastAPI
+
+from lib.llm import LLM
 
 app = FastAPI(title="ticup")
 
+
 @app.post("/chat")
 async def chat_endpoint(msg: str):
+    llm: LLM = LLM()
+    return llm.chat(msg)
+
+
+uvicorn.run(app, host="0.0.0.0", port=1515)
