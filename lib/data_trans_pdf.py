dc397cb1f7ce94477910cee15666819e0703e59a
Lntano-S <1932474243@qq.com>
2026-03-07 14:23:07 +0800

    Update

--- lib/data_trans_pdf.py
+++ lib/data_trans_pdf.py
@@ -1,20 +1,48 @@
 import pandas as pd
 import pdfplumber
 
-file_path: str = "示例数据/附件2：财务报告/reports-上交所/600080_20230428_FQ2V.pdf"
+
+class TransPDF:
+    def __init__(self) -> None:
+        self.tables: list[pd.DataFrame] = []
+
+    def trans_pdf(self, file_path: str) -> list[pd.DataFrame]:
+        with pdfplumber.open(file_path) as pdf:
+            pages: list = pdf.pages
+
+            for page in pages:
+                table = page.extract_table()
+
+                if table:
+                    df: pd.DataFrame = pd.DataFrame(table)
+                    self.tables.append(df)
+
+        return self.tables
+
+    def clean_format_pdf(self, file_path: str):
+        for table in self.tables:
+            clean_table: pd.DataFrame = table
+            
+
+    def print_tables(self) -> None:
+        count: int = 1
+
+        if self.tables:
+            for table in self.tables:
+                print(f"第{count}个表格")
+                print(table)
+
+                count += 1
+        else:
+            print("没有表格！！")
 
 
-def trans_pdf(file_path: str) -> list[pd.DataFrame]:
-    tables: list[pd.DataFrame] = []
+def main() -> None:
+    file_path: str = "示例数据/附件2：财务报告/reports-上交所/600080_20230428_FQ2V.pdf"
+    transpdf = TransPDF()
+    transpdf.trans_pdf(file_path)
+    transpdf.print_tables()
 
-    with pdfplumber.open(file_path) as pdf:
-        pages: list = pdf.pages
 
-        for page in pages:
-            table = page.extract_table()
-
-            if table:
-                df: pd.DataFrame = pd.DataFrame(table)
-                tables.append(df)
-
-    return tables
+if __name__ == "__main__":
+    main()
